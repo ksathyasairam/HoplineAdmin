a38a09d26724bfdb47c1d1e3336edcc159056a1e
Akshansh986 <akshanshsingh@gmail.com>
2016-12-06 03:25:41 +0530

    Navigation bar not showing fix.

--- app/src/main/java/com/example/ssairam/hopline/activity_ui/MainActivity.java
+++ app/src/main/java/com/example/ssairam/hopline/activity_ui/MainActivity.java
@@ -5,13 +5,9 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.FragmentManager;
-import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.PopupMenu;
-import android.widget.PopupWindow;
 import android.widget.Toast;
 
 import com.aurelhubert.ahbottomnavigation.AHBottomNavigation;
@@ -22,10 +18,6 @@ import com.example.ssairam.hopline.InitialiseDataFromServer;
 import com.example.ssairam.hopline.PrinterConnector;
 import com.example.ssairam.hopline.PrinterHelper;
 import com.example.ssairam.hopline.R;
-import com.example.ssairam.hopline.activity_ui.FeedbackForm;
-import com.example.ssairam.hopline.activity_ui.Inventory;
-import com.example.ssairam.hopline.activity_ui.OfflineOrder;
-import com.example.ssairam.hopline.activity_ui.OrderHistoryActivity;
 import com.example.ssairam.hopline.fragments.BigOrderPayFragment;
 import com.example.ssairam.hopline.fragments.IncomingOrderFragment;
 import com.example.ssairam.hopline.fragments.NewOrderFragment;
@@ -43,18 +35,6 @@ public class MainActivity extends BaseActivity implements IncomingOrderFragment.
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-//        View decorView = getWindow().getDecorView();
-//// Hide both the navigation bar and the status bar.
-//// SYSTEM_UI_FLAG_FULLSCREEN is only available on Android 4.1 and higher, but as
-//// a general rule, you should design your app to hide the status bar whenever you
-//// hide the navigation bar.
-//        int uiOptions =  View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-//                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-//                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-//                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // hide nav bar
-//                | View.SYSTEM_UI_FLAG_FULLSCREEN // hide status bar
-//                | View.SYSTEM_UI_FLAG_IMMERSIVE;
-//        decorView.setSystemUiVisibility(uiOptions);
         setContentView(R.layout.activity_main);
 
         if (findViewById(R.id.fragment_container) != null) {
@@ -96,7 +76,7 @@ public class MainActivity extends BaseActivity implements IncomingOrderFragment.
     }
 
     private void initUi() {
-        intializenavigation();
+        initBottomNavigationBar();
 
         if (findViewById(R.id.fragment_container) != null) {
             IncomingOrderFragment Fragment = new IncomingOrderFragment();
@@ -109,7 +89,8 @@ public class MainActivity extends BaseActivity implements IncomingOrderFragment.
 
     }
 
-    private void intializenavigation() {
+    private void initBottomNavigationBar() {
+        if (bottomNavigation != null) return;
 
         bottomNavigation = (AHBottomNavigation) findViewById(R.id.bottom_navigation);
 
@@ -284,8 +265,7 @@ public class MainActivity extends BaseActivity implements IncomingOrderFragment.
 
     @Override
     protected void onResume() {
-
-
+        initBottomNavigationBar();
         super.onResume();
     }
 }
